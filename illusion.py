3d6288c1a4b5210770ba925715b65e53ec8c9e04
Estudio <jfsuarezb@icloud.com>
2019-08-17 13:34:37 -0500

    Added gradual color change to moving object

--- illusion.py
+++ illusion.py
@@ -17,15 +17,21 @@ if __name__ == "__main__":
     tk.title("illusion")
     canvas.pack()
     center_point = canvas.create_oval(400, 400, 405, 405, fill="black")
-    ball = canvas.create_oval(400, 10, 405, 15, fill="orange")
+    ball = canvas.create_oval(400, 10, 420, 30, fill="gray99")
     deg = 0
+    i = 11
+    j = 1
     while True:
+        if i == 99 or i == 10:
+            j = -j
         if deg == 360:
             deg = 0
+        canvas.itemconfig(ball, fill="gray"+str(i))
         posball = canvas.coords(ball)
         poscenter = canvas.coords(center_point)
         movex, movey = give_move(posball[0], posball[1], poscenter[0], poscenter[1], deg)
         canvas.move(ball, movex, movey)
         tk.update()
         deg = deg + 0.5
+        i = i+j
     tk.mainloop()
